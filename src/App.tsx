44b54f56e2c82ef98e05aa15efee88c3b3e58ca9
tiendung06 <dungdotien14@gmail.com>
2024-12-14 08:50:30 +0700

    Add call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,62 @@
 import React, { useState } from "react";
 import { FiUpload } from "react-icons/fi";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import "./App.scss";
+
+type TData = {
+  card_id: string;
+  name: string;
+  dob: string;
+  address: string;
+  home: string;
+};
 
 const App: React.FC = () => {
   const [frontImage, setFrontImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>();
 
-  const handleImageUpload = (
+  const { register, setValue, reset } = useForm<TData>();
+
+  const handleImageUpload = async (
     event: React.ChangeEvent<HTMLInputElement>,
     setImage: React.Dispatch<React.SetStateAction<string | null>>
   ) => {
     const file = event.target.files?.[0];
     if (file) {
+      // Preview ảnh
       const reader = new FileReader();
       reader.onload = (e) => {
         setImage(e.target?.result as string);
       };
       reader.readAsDataURL(file);
+
+      // Tạo FormData và gọi API
+      try {
+        setLoading(true);
+        reset();
+        const formData = new FormData();
+        formData.append("image", file, file.name);
+
+        const response = await axios.post("http://116.104.73.112:10004/ocr/v1.0/gttt_extractor", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        console.log("Response:", response.data);
+        if (response.data.data) {
+          setValue("name", response.data.data.name.replace(/\f/g, ""));
+          setValue("card_id", response.data.data.card_id.replace(/\f/g, ""));
+          setValue("dob", response.data.data.dob.replace(/\f/g, ""));
+          setValue("address", response.data.data.address.replace(/\f/g, ""));
+          setValue("home", response.data.data.home.replace(/\f/g, ""));
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        setLoading(false);
+      }
     }
   };
 
@@ -39,29 +80,36 @@ const App: React.FC = () => {
             </div>
           </div>
 
-          <div className="flex-1 p-4 pb-10 rounded-lg shadow-md content-right">
-            <div className="grid grid-cols-1 gap-5 field-group md:grid-cols-2 xl:grid-cols-3">
+          <div className="relative flex-1 p-4 pb-10 rounded-lg shadow-md content-right">
+            <div
+              className={`${
+                !loading && "hidden"
+              } fixed inset-0 flex items-center bg-opacity-25 justify-center bg-black overlay z-30`}
+            >
+              <div className="w-12 h-12 border-4 border-blue-500 rounded-full border-t-transparent animate-spin"></div>
+            </div>
+            <form className="grid grid-cols-1 gap-5 field-group md:grid-cols-2 xl:grid-cols-3">
               <div className="field">
                 <label htmlFor="fullName">Fullname</label>
-                <input type="text" id="fullName" placeholder="" disabled />
+                <input type="text" id="fullName" placeholder="" disabled {...register("name")} />
               </div>
               <div className="field">
                 <label htmlFor="idCard">Citizen number</label>
-                <input type="text" id="idCard" placeholder="" disabled />
+                <input type="text" id="idCard" placeholder="" disabled {...register("card_id")} />
               </div>
               <div className="field">
                 <label htmlFor="dateOfBirth">Date of birth</label>
-                <input type="text" id="dateOfBirth" placeholder="" disabled />
+                <input type="text" id="dateOfBirth" placeholder="" disabled {...register("dob")} />
               </div>
               <div className="field">
-                <label htmlFor="gender">Gender</label>
-                <input type="text" id="gender" placeholder="" disabled />
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" placeholder="" disabled {...register("address")} />
               </div>
               <div className="field">
-                <label htmlFor="nationality">Nationality</label>
-                <input type="text" id="nationality" placeholder="" disabled />
+                <label htmlFor="home">Home</label>
+                <input type="text" id="home" placeholder="" disabled {...register("home")} />
               </div>
-              <div className="field">
+              {/* <div className="field">
                 <label htmlFor="origin">Place of origin</label>
                 <input type="text" id="origin" placeholder="" disabled />
               </div>
@@ -80,8 +128,8 @@ const App: React.FC = () => {
               <div className="field">
                 <label htmlFor="provider">Provider</label>
                 <input type="text" id="provider" placeholder="" disabled />
-              </div>
-            </div>
+              </div> */}
+            </form>
           </div>
         </div>
       </div>
